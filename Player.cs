3fed1828800f4fd014ef8b3b62407a7330e09669
Francis <francis.paddock@icloud.com>
2018-12-14 10:49:29 +0000

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,29 +1,28 @@
 ﻿using System.Collections; using System.Collections.Generic; using UnityEngine;  public class Player : Character
 {
     float speed = 4.0f;
-    Vector3 location;
-    Transform tran;
+    Vector3 location;    // Vector3 is named 'location'
+    Transform t;         // Transform is named 't'      /* 'Transform' is its own datatype.It's a class, which means you can call       * methods with a full stop like Transform.position or  Transform.translate */
      private bool idle;
 
     void Start()
     {
-        location = transform.position;
-        tran = transform;          idle = true;
+        t = transform; // Transform = transform = t         location = transform.position; // transform.position is the .position method being used on the transform/Transform/t class.          idle = true;
     }
 
     void Update()
     {
 
-        if (Input.GetKey(KeyCode.RightArrow) && tran.position == location && (idle = true))
+        if (Input.GetKey(KeyCode.RightArrow) && t.position == location && (idle = true))
         {
             location += Vector3.right;             idle = false;
-        }          if (Input.GetKey(KeyCode.LeftArrow) && tran.position == location && (idle = true))
+        }          if (Input.GetKey(KeyCode.LeftArrow) && t.position == location && (idle = true))
         {
             location += Vector3.left;             idle = false;
-        }          if (Input.GetKey(KeyCode.UpArrow) && tran.position == location && (idle = true))
+        }          if (Input.GetKey(KeyCode.UpArrow) && t.position == location && (idle = true))
         {
             location += Vector3.up;             idle = false;
-        }          if (Input.GetKey(KeyCode.DownArrow) && tran.position == location && (idle = true))
+        }          if (Input.GetKey(KeyCode.DownArrow) && t.position == location && (idle = true))
         {
             location += Vector3.down;             idle = false;
         }
